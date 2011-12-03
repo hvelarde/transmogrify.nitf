84cfa0538e7e4606bf36b4e529f3b90286cf66ef
hvelarde <hector.velarde@gmail.com>
2011-12-02 19:19:54 -0600

    added testing infrastructure and tests from collective.nitf to package; tests need to be rewritten

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ setup(name='transmogrify.nitf',
         'collective.transmogrifier',
         'plone.app.transmogrifier',
         ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
