3581204f515033214fbbefc0b8c7aa6ae55bfea2
nueces <juan@linux.org.ar>
2012-04-19 19:58:38 -0300

    Added isodate to parse string dates. Removed method to contruct the path based on dates for archivement, it could be modified in the transmogrifier pipeline. Added new keywods into the item dict based on the default behaviours of the collectite.nift content type definition.

--- src/transmogrify/nitf/xmlsource.py
+++ src/transmogrify/nitf/xmlsource.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from isodate import parse_datetime
 import xml.etree.ElementTree as etree
 
 from zope.interface import classProvides, implements
@@ -25,16 +25,6 @@ def get_text(dom, subelemet, attribute=None):
     return ''
 
 
-def get_date_path(dom, subelemet, attribute):
-    """ Return a path 'YYYY/MM/DD' based on a date value normalized into
-        ISO-8601
-        Note: Only work with the basic format.
-    """
-    text = get_text(dom, subelemet, attribute)
-    # We only need the YYYYMMDD part from the string
-    return "/".join([text[:4], text[4:6], text[6:8]])
-
-
 class XMLSource(object):
     """ Process an string containing a xml representation of a nitf object.
     """
@@ -46,19 +36,27 @@ class XMLSource(object):
 
     def __iter__(self):
         for data in self.previous:
-            item = {'id': '',
+            item = {'_path': None,
                     '_type': 'collective.nitf.content',
-                    'path': '',
-                    'title': '',
-                    'subtitle': '',
-                    'description': '',
-                    'byline': '',
-                    'text': '',
-                    'genre': '',
-                    'section': '',
-                    'urgency': '',
-                    'location': '',
-                    'media': {'images': [],
+                    # plone.app.dexterity.behaviours.metadata.IBasic
+                    'title': None, 'description': None,
+                    # plone.app.dexterity.behaviours.metadata.ICategorization
+                    'subject': [], 'language': '',
+                    # plone.app.dexterity.behaviours.metadata.IPublication
+                    'effective': None, 'expires': None,
+                    # plone.app.dexterity.behaviours.metadata.IOwnership
+                    'creators': [], 'contributors': [], 'rights': None,
+                    # TODO: How the standar manage refenreces and related items.
+                    # plone.app.referenceablebehavior.referenceable.IReferenceable
+                    #'_plone.uuid': '',
+                    # plone.app.relationfield.behavior.IRelatedItems
+                    # 'relatedItems': (),
+                    # collective.nitf.content.INITF
+                    'subtitle': '', 'byline': '', 'text': '', 'genre': '',
+                    'section': '', 'urgency': '', 'location': '',
+                    # objects that should be created inside of the current
+                    # NITF object.
+                    '_media': {'images': [],
                               'videos': []}
                     }
 
@@ -66,29 +64,43 @@ class XMLSource(object):
             head = dom.find('head')
             body = dom.find('body')
 
-            item['id'] = get_text(head, 'docdata/doc-id', 'id-string').lower()
-            item['path'] = get_date_path(head, 'docdata/date.release', 'norm')
+            item['_path'] = get_text(head, 'docdata/doc-id', 'id-string').lower()
             item['title'] = get_text(head, 'title')
+            item['subject'] = [k.get('key') for k in \
+                            list(head.find('docdata/key-list')) if k.get('key')]
+            #item['language']
+            sdate = get_text(head, 'docdata/date.release', 'norm')
+            if sdate:
+                item['effective'] = parse_datetime(sdate)
+
+            sdate = get_text(head, 'docdata/date.expire', 'norm')
+            if sdate:
+                item['expires'] = parse_datetime(sdate)
+
+            #sdate = get_text(head, 'docdata/date.issue', 'norm')
+            #if sdate:
+            #    item['issue'] = parse_datetime(sdate)
+
             item['genre'] = get_text(head, 'tobject/tobject.property',
-                                           'tobject.property.type')
+                                    'tobject.property.type')
             item['section'] = get_text(head, 'pubdata', 'position.section')
             item['urgency'] = get_text(head, 'docdata/urgency', 'ed-urg')
 
+            item['description'] = get_text(body, 'body.head/abstract/p')
             item['location'] = get_text(body, 'body.head/dateline/location')
             item['subtitle'] = get_text(body, 'body.head/hedline/hl2')
-            item['description'] = get_text(body, 'body.head/abstract')
             item['byline'] = get_text(body, 'body.head/byline/person')
 
             for elem in list(body.find('body.content')):
                 if elem.tag == 'media' and elem.get('media-type') == 'image':
                     image = elem.find('media-reference').attrib
                     image['media-caption'] = get_text(elem, 'media-caption')
-                    item['media']['images'].append(image)
+                    item['_media']['images'].append(image)
 
                 elif elem.tag == 'media' and elem.get('media-type') == 'video':
                     video = elem.find('media-reference').attrib
                     video['media-caption'] = get_text(elem, 'media-caption')
-                    item['media']['videos'].append(video)
+                    item['_media']['videos'].append(video)
 
                 else:   # other tag are considered part of the body text and
                         # should be preserved.
